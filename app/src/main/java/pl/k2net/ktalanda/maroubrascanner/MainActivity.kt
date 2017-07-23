5a3104a9267df2005abb197c0403deb3d2abd5a7
ktalanda <ktalanda@k2net.pl>
2017-07-23 12:32:18 +1000

    Initial commit

---
+++ app/src/main/java/pl/k2net/ktalanda/maroubrascanner/MainActivity.kt
@@ -0,0 +1,13 @@
+package pl.k2net.ktalanda.maroubrascanner
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
