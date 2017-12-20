b8eeee9c6c4f2255b52b6537d729f23014372234
ktalanda <ktalanda@k2net.pl>
2017-12-20 15:30:56 +0100

    Create sample todoapp.

--- app/src/main/java/pl/k2net/ktalanda/redux/example/MainActivity.kt
+++ app/src/main/java/pl/k2net/ktalanda/redux/example/MainActivity.kt
@@ -2,12 +2,38 @@ package pl.k2net.ktalanda.redux.example
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.ListView
+import butterknife.BindView
+import butterknife.ButterKnife
+import butterknife.OnClick
+import butterknife.Unbinder
 
 class MainActivity : Activity() {
 
+    @BindView(R.id.todoInput) lateinit var todoInputView: EditText
+    @BindView(R.id.todoList) lateinit var todoListView: ListView
+
+    private lateinit var unbinder: Unbinder
+
+    private var todoList = listOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
+        unbinder = ButterKnife.bind(this)
+    }
+
+    override fun onDestroy() {
+        unbinder.unbind()
+        super.onDestroy()
+    }
+
+    @OnClick(R.id.addTodo)
+    fun addTodo() {
+        todoList += todoInputView.text.toString()
+        todoListView.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, todoList)
+        todoInputView.text.clear()
     }
 }
